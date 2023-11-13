e54fc5f06b6245d6d8740e131efd48d71a998688
Chris <117781333+chrism-la@users.noreply.github.com>
2023-11-13 12:53:14 -0800

    Updated .env file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
-app.listen(3000);
+app.listen(process.env.PORT);
