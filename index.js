473ee5a6561097c159dac7195c651d7359bbd3fb
Chris <117781333+chrism-la@users.noreply.github.com>
2023-11-13 13:07:27 -0800

    Styled wildcard route

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
-app.get('*', (req, res) => {});
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page</h1>');
+});
 app.listen(process.env.PORT);
