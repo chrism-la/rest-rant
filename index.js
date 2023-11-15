0e2153d1f892ce1a7ae52d304fa17da6ea082273
Chris <117781333+chrism-la@users.noreply.github.com>
2023-11-15 15:47:09 -0800

    Organized index.js entry point file with comments

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// Modules and Globals
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
+// Express Settings
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
+
+// Controllers & Routes
 app.use('/places', require('./controllers/places'));
 app.get('/', (req, res) => {
     res.render('home');
@@ -10,4 +16,6 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
     res.status(404).render('error404');
 });
+
+// Listen for connections
 app.listen(process.env.PORT);
