38c690fafad48d43aba6402c102a9af8c764f525
Chris <117781333+chrism-la@users.noreply.github.com>
2023-11-15 20:14:46 -0800

    Edited places folder

--- controllers/places.js
+++ controllers/places.js
@@ -1,34 +1,26 @@
 const router = require('express').Router();
+const places = require('../models/places.js');
 
 router.get('/new', (req, res) => {
     res.render('places/new');
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('POST /places');
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400';
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown';
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA';
+    }
+    places.push(req.body);
+    res.redirect('/places');
 });
 
 router.get('/', (req, res) => {
-    let places = [
-        {
-            id: 1,
-            name: 'H-Thai-ML',
-            city: 'Seattle',
-            state: 'WA',
-            cuisines: 'Thai, Pan-Asian',
-            pic: '/images/asianbowl.jpg',
-        },
-        {
-            id: 2,
-            name: 'Coding Cat Cafe',
-            city: 'Phoenix',
-            state: 'AZ',
-            cuisines: 'Coffee, Bakery',
-            pic: '/images/pizza.jpg',
-        },
-    ];
-
     res.render('places/index', { places });
 });
 
---
+++ models/places.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Healthy Bowls',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/asianbowl.jpg',
+    },
+    {
+        id: 2,
+        name: 'Pizzario',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: '/images/pizza.jpg',
+    },
+];
