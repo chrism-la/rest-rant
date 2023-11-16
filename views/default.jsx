0ad57326365c3f4193b89fbec2917bc17fec2f9c
Chris <117781333+chrism-la@users.noreply.github.com>
2023-11-15 16:10:31 -0800

    capitalization typo in CSS path

--- views/default.jsx
+++ views/default.jsx
@@ -5,12 +5,8 @@ function Def(html) {
         <html>
             <head>
                 <title>Title</title>
-                <link
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-                    rel="stylesheet"
-                    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-                ></link>
-                <link rel="stylesheet" href="public\CSS\styles.css"></link>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+                <link href="/CSS/styles.css" rel="stylesheet"></link>
             </head>
             <body>{html.children}</body>
         </html>
