7743468e3312b2859b9818c9d9430bd60f94c870
Chris <117781333+chrism-la@users.noreply.github.com>
2023-12-02 10:21:45 -0800

    fixed edit route

--- controllers/places.js
+++ controllers/places.js
@@ -47,12 +47,17 @@ router.delete('/:id', (req, res) => {
 
 router.get('/:id/edit', (req, res) => {
     let id = Number(req.params.id);
+    console.log(id);
+
     if (isNaN(id)) {
         res.render('error404');
     } else if (!places[id]) {
         res.render('error404');
     } else {
-        res.render('places/edit', { place: places[id], id });
+        res.render('places/edit', {
+            place: places[req.params.id],
+            index: req.params.id,
+        });
     }
 });
 
